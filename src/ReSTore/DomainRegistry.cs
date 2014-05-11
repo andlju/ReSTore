d714aee43d25abaf5aea13114449f9b6e522af86
Anders Ljusberg <andlju@gmail.com>
2014-05-11 21:49:54 +0200

    Can now build view-models in-process

--- src/ReSTore/DomainRegistry.cs
+++ src/ReSTore/DomainRegistry.cs
@@ -3,22 +3,37 @@ using ReSTore.Domain;
 using ReSTore.Domain.CommandHandlers;
 using ReSTore.Domain.Services;
 using ReSTore.Infrastructure;
+using ReSTore.Views.Builders;
 using StructureMap.Configuration.DSL;
 
 namespace ReSTore
 {
+    public class PricingServiceRegistry : Registry
+    {
+        public PricingServiceRegistry()
+        {
+            IncludeRegistry<RavenProductsRegistry>();
+            For<IPricingService>().Singleton().Use<PricingService>();
+        }
+    }
+
     public class DomainRegistry : Registry
     {
         public DomainRegistry()
         {
-            IncludeRegistry<RavenProductsRegistry>();
-            For<IPricingService>().Singleton().Use<PricingService>();
+            IncludeRegistry<PricingServiceRegistry>();
+            IncludeRegistry<ViewBuilderRegistry>();
             Scan(s =>
                 {
                     s.AssemblyContainingType<CreateOrderHandler>();
                     s.ConnectImplementationsToTypesClosing(typeof (ICommandHandler<>));
                 });
-            For<IRepository<Guid>>().Singleton().Use<EventStoreRepository>();
+            For<IModelUpdateNotifier>().Use<NullModelUpdateNotifier>();
+            For<IRepository<Guid>>().Singleton().Use<EventStoreRepository>().OnCreation("Registering ViewBuilders",
+                (ctxt, repository) =>
+                {
+                    // repository.RegisterDispatcher(ctxt.GetInstance<ViewBuilderEventDispatcher>());
+                });
         }
     }
 }
